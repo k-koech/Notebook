8e63d9bf7e411b0b84ebc560c4105b489251cabf
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-23 14:57:47 +0300

    updated config file

---
+++ config.py
@@ -0,0 +1,50 @@
+import os 
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = '<Flask WTF Secret Key>'
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com' 
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = "triplek901@gmail.com"
+    MAIL_PASSWORD ="kelvin97"   
+    MAIL_DEBUG = True
+    MAIL_SUPPRESS_SEND = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+ 
+
+class TestConfig(Config):
+    pass
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}

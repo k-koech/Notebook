902356c3c3f67f8825ef46c2b05caeb27d3033f6
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-23 07:35:14 +0300

    updated createdb file

--- app/__init__.py
+++ app/__init__.py
@@ -52,7 +52,8 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-  
+    
+    
 
     return app
 
--- db_create.py
+++ db_create.py
@@ -1,2 +1,4 @@
-from app import db
-db.create_all()
+from app import db, app
+
+with app.app_context():
+    db.create_all()

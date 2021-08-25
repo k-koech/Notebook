f7e3732ac14cc60a824c37097a08011fc5464be5
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-26 00:58:22 +0300

    added main forms

---
+++ app/main/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms.validators import DataRequired
+from wtforms.validators import Required, ValidationError, Length, Email
+from flask_login import current_user
+from ..models import User
+
+class NotesForm(FlaskForm):
+    title = StringField('Title',validators=[DataRequired()])
+    notes = TextAreaField('Notes',validators=[DataRequired()])
+    submit = SubmitField('Save Notes',validators=[DataRequired()])
+
+class UpdateProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=15)])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    picture=FileField("Update Profile Picture", validators=[FileAllowed(['jpg','png','jpeg'])] )   
+    submit = SubmitField('Update Profile')
+
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+           user = User.query.filter_by(username=username.data).first()
+           if user:
+               raise ValidationError("Username Taken")
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("Email has already been registered")

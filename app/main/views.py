412f5e2a02066d557d817b54fd52d90626e72802
k-koech <kelvin.koech@student.moringaschool.com>
2021-08-26 00:57:13 +0300

    added views file

---
+++ app/main/views.py
@@ -0,0 +1,38 @@
+from flask import render_template,request,redirect,url_for, abort, flash
+from . import main
+from sqlalchemy import asc
+from ..models import  Feedback, Pitch, User, Comments
+from flask_login import login_required, current_user
+from .forms import NotesForm,UpdateProfileForm
+from .. import db
+from PIL import Image
+import os, secrets
+
+
+# Views
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html', title ="Home")
+
+
+"""NEW NOTES VIEW"""
+@main.route('/notes/new', methods = ['GET','POST'])
+# @login_required
+def new_note():
+    form = NotesForm()
+    return render_template('create_note.html', title = "New Note", form=form)
+
+
+"""PROFILE NOTES VIEW"""
+@main.route('/profile/<username>', methods = ['GET','POST'])
+# @login_required
+def profile(username):
+    '''
+    Profile view to update profile
+    '''   
+    form=UpdateProfileForm()
+    return render_template('profile.html', title = "New Note", form=form)
+

8c21d0c8c8b476d706738606743b984317dcd381
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-08-26 14:59:19 +0300

    function to update notes

--- app/main/views.py
+++ app/main/views.py
@@ -45,3 +45,20 @@ def delete_notes(id):
     db.session.commit()
     flash("Notes deleted", "success")
     return redirect(url_for("main.index"))
+"""UPDATE NOTES"""
+@main.route('/notes/update/<id>', methods = ['GET','POST'])
+@login_required
+def update_notes(id):
+    notes = Notes.query.get_or_404(id)
+    form = NotesForm()
+    if form.validate_on_submit():
+        notes.title = form.title.data
+        notes.notes = form.notes.data
+        db.session.commit()
+        flash("Changes saved successfully", "success")
+        return redirect(url_for('main.notes', id=notes.id))
+    elif request.method == "GET":
+        form.title.data = notes.title
+        form.notes.data = notes.notes
+        title = "Update Post"
+        return render_template("create_note.html", title=title, legend="Update Notes",form=form,)

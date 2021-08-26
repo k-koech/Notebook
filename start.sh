4f1ab20033d26197ff41dc94c1403a612eccf2ae
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-08-26 14:52:27 +0300

    update index.html

--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@ export SECRET_KEY='vsegsetge3'
 export MAIL_USERNAME='skyles171@gmail.com'
 export MAIL_PASSWORD='SKlassen'
 
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:skyles171@localhost/notebook'
 python3 manage.py server

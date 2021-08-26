faee81a93d1a252f1a461f9cf9a8e2d352900e47
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-08-26 10:17:45 +0300

    email configurations

--- config.py
+++ config.py
@@ -6,6 +6,13 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = 'jihesgvuys'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
  
 
 class TestConfig(Config):
@@ -29,7 +36,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:kkkk@localhost/notebook'
+    
     DEBUG = True
 
 config_options = {
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY='vsegsetge3'
-
+export MAIL_USERNAME='skyles171@gmail.com'
+export MAIL_PASSWORD='SKlassen'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:kkkk@localhost/notebook'
 python3 manage.py server

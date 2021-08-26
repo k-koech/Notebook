ed2ea07ebb20b4e6472bf11882463a73cff85f75
eude313 <eudes.charles@student.moringaschool.com>
2021-08-26 12:58:32 +0300

    add register

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,19 +7,20 @@ from .. import db, bcrypt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import current_user
 
-@auth.route('/register',methods = ["GET","POST"])
-def register():
+@auth.route('/login',methods = ["GET","POST"])
+def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
+        return redirect(url_for('main.index'))
+    form = LoginForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username = form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash("Your account has been created successfully!", "success")
-        return redirect(url_for('auth.login'))
-    return render_template("auth/register.html", title="Register", form=form)
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for("main.index"))
+        else:
+            flash(f"Invalid credentials", "danger")
+    return render_template("auth/login.html", title="Login", form=form)
 
 @auth.route('/logout')
 @login_required

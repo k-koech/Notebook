f306dabd4e18116823678700e57f2f46f05fefa8
eude313 <eudes.charles@student.moringaschool.com>
2021-08-26 12:58:55 +0300

    add all changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,6 +7,20 @@ from .. import db, bcrypt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import current_user
 
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Your account has been created successfully!", "success")
+        return redirect(url_for('auth.login'))
+    return render_template("auth/register.html", title="Register", form=form)
+
 @auth.route('/login',methods = ["GET","POST"])
 def login():
     if current_user.is_authenticated:

d6b9cf192547f1187b54e976f434133ef8fa7809
eude313 <eudes.charles@student.moringaschool.com>
2021-08-26 12:57:46 +0300

    add login

--- app/auth/views.py
+++ app/auth/views.py
@@ -21,18 +21,9 @@ def register():
         return redirect(url_for('auth.login'))
     return render_template("auth/register.html", title="Register", form=form)
 
-@auth.route('/login',methods = ["GET","POST"])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                next_page = request.args.get('next')
-                return redirect(next_page) if next_page else redirect(url_for("main.index"))
-        else:
-            flash(f"Invalid credentials", "danger")
-    return render_template("auth/login.html", title="Login", form=form)
-
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash(f"Logout success!", "success")
+    return redirect(url_for("auth.login"))
